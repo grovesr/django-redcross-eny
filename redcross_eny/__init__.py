11270e60419844c291bec59851ec407f5dcbccbf
Rob <grovesr1@yahoo.com>
2015-11-11 07:28:59 -0500

    add help on product code explanation and generation

--- redcross_eny/__init__.py
+++ redcross_eny/__init__.py
@@ -2,4 +2,5 @@
 # date format = dd/mm/YYYY
 # comment will be used to add comment to GIT tag
 #VERSION=('1.2.0','02/11/2015','add version, admin. server settings')
-VERSION=('1.3.0','10/11/2015','allow auto generation of product codes. Add date and comments to version')
+#VERSION=('1.3.0','10/11/2015','allow auto generation of product codes. Add date and comments to version')
+VERSION=('1.3.1','12/11/2015','add help on product code explanation and generation')
