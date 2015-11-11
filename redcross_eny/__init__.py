7b9267cbbadf4b52299753454097cbfe79d6a497
Rob <grovesr1@yahoo.com>
2015-11-10 22:05:06 -0500

    add date and comments to version information

--- redcross_eny/__init__.py
+++ redcross_eny/__init__.py
@@ -2,4 +2,4 @@
 # date format = dd/mm/YYYY
 # comment will be used to add comment to GIT tag
 #VERSION=('1.2.0','02/11/2015','add version, admin. server settings')
-VERSION = '1.2.1'
+VERSION=('1.3.0','10/11/2015','allow auto generation of product codes. Add date and comments to version')
