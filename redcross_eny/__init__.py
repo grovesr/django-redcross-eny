68d0e3331366f6518005a33c0cf06249bd7cf00d
Rob <grovesr1@yahoo.com>
2015-11-21 15:13:12 -0500

    add TEMP_DIR to settings

--- redcross_eny/__init__.py
+++ redcross_eny/__init__.py
@@ -3,4 +3,5 @@
 # comment will be used to add comment to GIT tag
 #VERSION=('1.2.0','02/11/2015','add version, admin. server settings')
 #VERSION=('1.3.0','10/11/2015','allow auto generation of product codes. Add date and comments to version')
-VERSION=('1.3.1','12/11/2015','add help on product code explanation and generation')
+#VERSION=('1.3.1','12/11/2015','add help on product code explanation and generation')
+VERSION=('1.3.3','21/11/2015','add TEMP_DIR to settings')
