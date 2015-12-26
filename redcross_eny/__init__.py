a47ab8fbdae425ce8d75bcf1b8b81cfca3e24021
Rob <grovesr1@yahoo.com>
2015-12-26 09:49:24 -0500

    feat: Update for paging and small style updates.

--- redcross_eny/__init__.py
+++ redcross_eny/__init__.py
@@ -4,4 +4,5 @@
 #VERSION=('1.2.0','02/11/2015','add version, admin. server settings')
 #VERSION=('1.3.0','10/11/2015','allow auto generation of product codes. Add date and comments to version')
 #VERSION=('1.3.1','12/11/2015','add help on product code explanation and generation')
-VERSION=('1.3.3','21/11/2015','add TEMP_DIR to settings')
+#VERSION=('1.3.3','21/11/2015','add TEMP_DIR to settings')
+VERSION=('1.4.0','26/12/2015','feat: Update for paging and small style updates.')
